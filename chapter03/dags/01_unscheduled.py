6a3f3eb2471738fba080a09eb8cf766c8346f465
danniel.kil <danniel.kil@ip-10-23-9-168.ap-northeast-2.compute.internal>
2025-02-25 19:57:12 +0900

    test completed

--- chapter03/dags/01_unscheduled.py
+++ chapter03/dags/01_unscheduled.py
@@ -10,13 +10,16 @@ dag = DAG(
     dag_id="01_unscheduled", start_date=datetime(2019, 1, 1), schedule_interval=None
 )
 
+# curl -o [저장할_파일명] [다운로드할_URL]
 fetch_events = BashOperator(
     task_id="fetch_events",
     bash_command=(
         # "mkdir -p /data/events && "
         # "curl -o /data/events.json http://events_api:5000/events"
+        "mkdir -p /opt/airflow/data && "
+        "curl -o /opt/airflow/data/events.json https://dev-web-213242029674.us-central1.run.app/api/getIP"
         # "mkdir /Users/danniel.kil/Documents/workspace/airflow/chapter03/data/events && "
-        "curl -o /Users/danniel.kil/Documents/workspace/airflow/chapter03/data/events.json http://events_api:5000/events"
+        # "curl -o /Users/danniel.kil/Documents/workspace/airflow/chapter03/data/events.json http://events_api:5000/events"
     ),
     dag=dag,
 )
@@ -25,18 +28,21 @@ fetch_events = BashOperator(
 def _calculate_stats(input_path, output_path):
     """Calculates event statistics."""
 
+    print("1")
     Path(output_path).parent.mkdir(exist_ok=True)
-
+    print("2")
     events = pd.read_json(input_path)
+    print("3")
     stats = events.groupby(["date", "user"]).size().reset_index()
-
+    print("4")
     stats.to_csv(output_path, index=False)
+    print("5")
 
 
 calculate_stats = PythonOperator(
     task_id="calculate_stats",
     python_callable=_calculate_stats,
-    op_kwargs={"input_path": "/data/events.json", "output_path": "/data/stats.csv"},
+    op_kwargs={"input_path": "/opt/airflow/data/events.json", "output_path": "/opt/airflow/data/stats.csv"},
     dag=dag,
 )
 
